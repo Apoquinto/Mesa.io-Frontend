7113f013ef52ca4c819f6cc40dc2c1e6a2036d96
Fernando Canul Caballero <121654243+elegidocodes@users.noreply.github.com>
2023-04-18 04:01:16 -0600

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +7,12 @@ import { HomeclientComponent } from './pages/homeclient/homeclient.component';
 import { MenuclientComponent } from './pages/menuclient/menuclient.component';
 import { BannermenuComponent } from './components/bannermenu/bannermenu.component';
 import { ReservacionesComponent } from './components/reservaciones/reservaciones.component';
+import { ManagerMenu } from './components/managermenu/managermenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ManagerMenu,
     ReservacionesComponent,
     AppComponent,
     NavbarComponent,
@@ -19,6 +21,7 @@ import { ReservacionesComponent } from './components/reservaciones/reservaciones
     BannermenuComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
