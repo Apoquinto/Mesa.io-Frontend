870cf4037666e775fab1838662a7c07fbd889db7
Fernando Canul Caballero <121654243+elegidocodes@users.noreply.github.com>
2023-05-14 01:53:22 -0600

    Update

--- src/app/components/bannermenu/bannermenu.component.ts
+++ src/app/components/bannermenu/bannermenu.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bannermenu',
   templateUrl: './bannermenu.component.html',
-  styleUrls: ['./bannermenu.component.css']
 })
 export class BannermenuComponent {
 
