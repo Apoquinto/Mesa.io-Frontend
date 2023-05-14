157c18192c8c4e6e7d19c98ff7611bd4ff838dd8
Fernando Canul Caballero <121654243+elegidocodes@users.noreply.github.com>
2023-05-14 15:33:41 -0600

    Update

---
+++ src/app/pages/component-footer/component-footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-footer',
+  templateUrl: './component-footer.component.html',
+})
+export class ComponentFooterComponent {
+
+}
