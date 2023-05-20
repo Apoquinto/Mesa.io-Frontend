bf653fae227d24ecedc0799294bdcf106db1ff8e
Betoniix <miguelhumberto522@gmail.com>
2023-05-20 14:21:33 -0600

    cambios

--- src/app/components/loginclient/loginclient.component.ts
+++ src/app/components/loginclient/loginclient.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-loginclient',
   templateUrl: './loginclient.component.html',
 })
-export class LoginclientComponent {
-
-}
+export class LoginclientComponent {}
