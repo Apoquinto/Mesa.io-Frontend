19ab6c7482af53d12dcee820af431d73ee91c2be
Apoquinto <RCLlanesApo@gmail.com>
2023-05-21 18:19:29 -0600

    feat(components): generate dish preview component

---
+++ src/app/components/dish-preview/dish-preview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-preview',
+  templateUrl: './dish-preview.component.html',
+  styleUrls: ['./dish-preview.component.css']
+})
+export class DishPreviewComponent {
+
+}
