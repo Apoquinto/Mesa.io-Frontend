6a9cb0378efbc99025f6a37a1fadc18c548c0b5f
Apoquinto <RCLlanesApo@gmail.com>
2023-05-21 18:42:20 -0600

    feat(dishes): add base maquetation of dish preview

--- src/app/components/dish-preview/dish-preview.component.ts
+++ src/app/components/dish-preview/dish-preview.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DishPreviewDTO } from 'src/app/dishes/dish-preview.dto';
 
 @Component({
-  selector: 'app-dish-preview',
+  selector: 'dish-preview',
   templateUrl: './dish-preview.component.html',
-  styleUrls: ['./dish-preview.component.css']
+  styleUrls: ['./dish-preview.component.css'],
 })
 export class DishPreviewComponent {
+  @Input() dish: DishPreviewDTO;
 
+  constructor() {
+    this.dish = {
+      name: '',
+      description: '',
+      price: 0,
+      dishThumbnailURL: '',
+    };
+  }
 }
